76fdaa1b563fee787e47259b1c1cc924a67956cf
ChutikarnS <cchutikarnn@gmail.com>
2018-07-26 15:12:52 +0700

    First commit

---
+++ whatever/src/WorkshopStar.java
@@ -0,0 +1,43 @@
+
+public class WorkshopStar {
+
+    public static void main(String[] args) {
+        int start = 9;
+        // -----------------
+        System.out.println(" **   ** ");
+        // -----------------
+        for (int i = 0; i < 9; i++) {
+            if (i == start / 2) {
+                System.out.print(" ");
+            } else {
+                System.out.print("*");
+            }
+        }
+        newLine();
+        // -------------
+        for (int round = 0; round < 5; round++) {
+            generateSpace(round);
+            generateStar(start);
+            newLine();
+            start = start - 2;
+
+        }
+    }
+
+    private static void newLine() {
+        System.out.println();
+    }
+
+    private static void generateStar(int start) {
+        for (int i = 0; i < start; i++) {
+            System.out.print("*");
+        }
+    }
+
+    private static void generateSpace(int round) {
+        for (int a = 0; a < round; a++) {
+            System.out.print(" ");
+        }
+    }
+
+}
